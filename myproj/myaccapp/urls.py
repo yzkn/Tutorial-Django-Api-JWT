70b88c8ce57a7ef902fcda50de9b91f277c5f828
YA <YA-androidapp@users.noreply.github.com>
2020-03-23 23:21:52 +0900

    password-change

--- myproj/myaccapp/urls.py
+++ myproj/myaccapp/urls.py
@@ -6,4 +6,8 @@ app_name = 'myaccapp'
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('password_change/', views.PasswordChange.as_view(),
+         name='password_change'),
+    path('password_change/done/', views.PasswordChangeDone.as_view(),
+         name='password_change_done')
 ]
--- myproj/myaccapp/views.py
+++ myproj/myaccapp/views.py
@@ -1,9 +1,22 @@
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'myaccapp/signup.html'
+
+
+class PasswordChange(LoginRequiredMixin, PasswordChangeView):
+    success_url = reverse_lazy('myaccapp:password_change_done')
+    template_name = 'register/password_change_form.html'
+
+
+class PasswordChangeDone(LoginRequiredMixin, PasswordChangeDoneView):
+    template_name = 'register/password_changed_done.html'
